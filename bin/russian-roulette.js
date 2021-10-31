955f24cef1c684da59fdfd142c87ee41d58c13f1
Peter Juras <me@peterjuras.com>
2021-10-31 10:12:56 +0100

    Refactor module to ESM.

--- bin/russian-roulette.js
+++ bin/russian-roulette.js
@@ -1,23 +1,14 @@
 #! /usr/bin/env node
-const { promisify } = require("util");
-const rimraf = require("rimraf");
+import { promisify } from "node:util";
+import rimraf from "rimraf";
 
 const rimrafAsync = promisify(rimraf);
 
-async function main() {
-  const random = Math.floor(Math.random() * 7);
+const random = Math.floor(Math.random() * 7);
 
-  if (random === 0) {
-    await rimrafAsync("*");
-    console.log("*BANG*");
-  } else {
-    console.log("*Click*");
-  }
-}
-
-if (require.main === module) {
-  main().catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+if (random === 0) {
+  await rimrafAsync("*");
+  console.log("*BANG*");
+} else {
+  console.log("*Click*");
 }
